d23fc3be15da243e18a86bc0e0c081686fec217f
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-02-01 18:46:16 +0100

    fix: add user-agent to prevent some blockings (#8)
    * fix: add user-agent to prevent some blockings
    
    * chore: npm audit fix
    
    * Update action.yml
    
    * Update test.yml
    
    * cleanup

--- src/checks.ts
+++ src/checks.ts
@@ -1,32 +1,46 @@
-import fetch, { Response } from "node-fetch"
-import * as core from "@actions/core"
-import { parseUrl } from "./parseUrl"
+import fetch, { Response } from "node-fetch";
+import * as core from "@actions/core";
+import { parseUrl } from "./parseUrl";
 
-export { checks }
+export { checks };
 
 /**
  * Checks stats page for a given url
  */
-const checks = async (url: string | null, { minExpectedRegex, exactExpectedRegex }: { minExpectedRegex: string | undefined, exactExpectedRegex: string | undefined }) => {
-  if (!url || url == "null") {
-    return { grade: "F", url: "", uri: "" }
+const checks = async (
+  url: string | null,
+  {
+    minExpectedRegex,
+    exactExpectedRegex,
+  }: {
+    minExpectedRegex: string | undefined;
+    exactExpectedRegex: string | undefined;
   }
-  let baseUrl = "", uri = ""
+) => {
+  if (!url || url == "null") {
+    return { grade: "F", url: "", uri: "" };
+  }
+  let baseUrl = "",
+    uri = "";
   try {
     const parsedUrl = parseUrl(url);
     baseUrl = parsedUrl.baseUrl;
     uri = parsedUrl.uri;
   } catch (error) {
-    core.error(`Error while trying to parse URL ${url}: ${error}`)
-    return { grade: "F", url, uri: "" }
+    core.error(`Error while trying to parse URL ${url}: ${error}`);
+    return { grade: "F", url, uri: "" };
   }
 
-  const response = await fetch(url)
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    },
+  });
   return checkStatus(response);
 
-
   function checkStatus(response: Response) {
-    const grade = getGrade()
+    const grade = getGrade();
 
     return { grade, url: baseUrl, uri };
 
@@ -34,7 +48,10 @@ const checks = async (url: string | null, { minExpectedRegex, exactExpectedRegex
       if (response.ok) {
         if (!exactExpectedRegex || uri.match(new RegExp(exactExpectedRegex))) {
           return "A";
-        } else if (!minExpectedRegex || uri.match(new RegExp(minExpectedRegex))) {
+        } else if (
+          !minExpectedRegex ||
+          uri.match(new RegExp(minExpectedRegex))
+        ) {
           return "B";
         } else {
           return "C";
@@ -42,7 +59,9 @@ const checks = async (url: string | null, { minExpectedRegex, exactExpectedRegex
       } else if (response.status < 500) {
         return "F";
       } else {
-        throw new Error(`HTTP Error Response: ${response.status} ${response.statusText}`);
+        throw new Error(
+          `HTTP Error Response: ${response.status} ${response.statusText}`
+        );
       }
     }
   }
